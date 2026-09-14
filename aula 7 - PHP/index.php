425fdb48e8cb45cb81497ad690c9ba0c98ddd2a6
douglasrsilva33 <douglas.r.silva33@aluno.senai.br>
2026-09-14 11:27:35 -0300

    a.h,VFUKYEAW
    zjgvmf\sb

---
+++ aula 7 - PHP/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 2</title>
+</head>
+<body>
+    <h1>Somar dois valores</h1>
+<form method="get"></form>
+<label primeiro valor></label>
+<input type="number" name="valor1" required>
+
+<br><br>
+
+<label segundo valor></label>
+<input type="number" name="valor2" required>
+
+<br><br>
+<button type="
+submit">somar</button>
+
+<?php
+if (isset($_GET["valor1"]) && isset(($_GET["valor2"]))
+</body>
+</html>
