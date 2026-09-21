21e407346fe5d62fb888ec1871e0e97e4f9edc2a
douglasrsilva33 <douglas.r.silva33@aluno.senai.br>
2026-09-21 11:17:16 -0300

    ajfvw
    atividade 21/09/2026

--- aula 8 - PHP + HTML/IMC.php
+++ aula 8 - PHP + HTML/IMC.php
@@ -7,4 +7,18 @@ $imc = $peso / ($altura * $altura);
 
 echo "Seu IMC é: " . $imc;
 
+if ($imc < 18.5) {
+    echo "<p>Abaixo do peso</p>";
+} elseif ($imc <25) {
+    echo "<p>Peso normal</p>";
+} elseif ($imc < 30) {
+    echo "<p>Sobrepeso</P>";
+} elseif ($imc < 35) {
+    echo "<p>Obesidade grau 1</p>";
+} elseif ($imc < 40){
+    echo "<p>Obesidade grau 2</p>";
+} elseif ($imc > 40) {
+    echo "<p>Obesidade grau 3</p>"; 
+}  
+
 ?>
---
+++ aula 9 - PHP operadores/qtditens.php
@@ -0,0 +1,13 @@
+<?php
+if (isset($_GET["quantidade"]) && isset($_GET["valor"])) {
+    $quantidade = $_GET["quantidade"];
+    $valordoitem = $_GET["valor"];
+    $mul = $quantidade * $valordoitem;
+    echo = "valor total igual a $mult";
+}
+
+
+
+
+
+?>
